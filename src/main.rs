7c1bed3f8d25e3b19cece28ce265cc5a3c63878d
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-04 18:22:47 -0500

    program now accepts command line arguments and prints them to the screen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-#![allow(dead_code, unused_imports)]
-use std::env::args;
+#![allow(dead_code, unused_imports, unused_variables)]
+
+use std::env;
 fn main() {
-    println!("hello there")
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_name = &args[2];
+    println!("searching for {}", query);
+    println!("in {}", file_name);
 }
