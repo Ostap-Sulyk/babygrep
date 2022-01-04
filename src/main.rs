b44f3d19283f1671ab349d34eb436295aba13dbe
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-04 18:27:51 -0500

    Added reading from file capability

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![allow(dead_code, unused_imports, unused_variables)]
 
 use std::env;
+use std::fs;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let file_name = &args[2];
-    println!("searching for {}", query);
     println!("in {}", file_name);
+
+    let context = fs::read_to_string(file_name).expect("Something went wrong reading file");
+    println!("With text:\n{}", context)
 }
