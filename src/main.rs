af9d90252dd12d646840a30619d841016a794602
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-04 20:30:05 -0500

    refactored code to avoid 'primitive obsession'

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,24 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_name = &args[2];
-    println!("in {}", file_name);
 
-    let context = fs::read_to_string(file_name).expect("Something went wrong reading file");
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let context =
+        fs::read_to_string(config.filename).expect("\n\nSomething went wrong reading file\n\n");
     println!("With text:\n{}", context)
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config { query, filename }
 }
