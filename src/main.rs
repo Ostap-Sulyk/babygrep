ec4b1321008fea3011442158a3e4bb7116a2308f
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-05 09:48:16 -0500

    continued refactoring old code

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -15,14 +15,16 @@ fn main() {
         fs::read_to_string(config.filename).expect("\n\nSomething went wrong reading file\n\n");
     println!("With text:\n{}", context)
 }
-
 struct Config {
     query: String,
     filename: String,
 }
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
 }
+
