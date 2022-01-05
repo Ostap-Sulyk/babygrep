6948d91f16454093b1a3fb85517f2c5c4053e3b5
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-05 10:30:31 -0500

    changed return type of run funtion and made program more user friendly

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
-#![allow(dead_code, unused_imports, unused_variables)]
+#![allow(dead_code, unused_imports, unused_variables, unused_must_use)]
 
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("\nProblem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-
-    let context =
-        fs::read_to_string(config.filename).expect("\n\nSomething went wrong reading file\n\n");
-    println!("With text:\n{}", context)
+    run(config);
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let context = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", context);
+    Ok(())
+}
+
 struct Config {
     query: String,
     filename: String,
 }
+
 impl Config {
-    fn new(args: &[String]) -> Config {
-        Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("\n\tNot enouth arguments.");
+        }
+        Ok(Config {
             query: args[1].clone(),
             filename: args[2].clone(),
-        }
+        })
     }
 }
-
