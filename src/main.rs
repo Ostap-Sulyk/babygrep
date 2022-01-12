4b83c2dc875fb4f6d3d7b791fcd626106153fe2d
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-11 20:23:04 -0500

    now program brakes with a pretty error message and not some ugly one

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-#![allow(dead_code, unused_variables)]
-use std::{env, fs};
+#![allow(dead_code, unused_variables, unused_imports)]
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for: {}", &query);
-    println!("In file: {}", &filename);
+    let config = Config::new(&args).unwrap_or_else(|error|{
+    println!("Problem reading arguments: {}", error);
+    process::exit(-1);
+    });
+    println!("Searching for: {}", &config.query);
+    println!("In file: {}", &config.filename);
+}
 
-    let contents = fs::read_to_string(filename).expect("Error reading file");
-
-    println!("With contents: \n{}", contents);
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &Vec<String>) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
