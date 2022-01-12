a6dba4720f988d08b73df26f643bbc95c6a102b3
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-11 21:29:13 -0500

    extracted all parcing logic from main.rs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,25 @@
+use std::{fs, error::Error};
 
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("Contents: \n{}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &Vec<String>) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-#![allow(dead_code, unused_variables, unused_imports)]
-
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+use babygrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,32 +9,8 @@ fn main() {
     });
     println!("Searching for: {}", &config.query);
     println!("In file: {}", &config.filename);
-    if let Err(e) = run(config){
+    if let Err(e) = babygrep::run(config){
         println!("Application error: {}", e);
         process::exit(-1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("Contents: \n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &Vec<String>) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
     }
 }
