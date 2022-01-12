300272c39efa1a72adbee0100d37fd65846558eb
Ostap Sulyk <ostapsulyk@gmail.com>
2022-01-11 21:51:20 -0500

    program working

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+#![allow(unused_imports, unused_variables)]
+
 use std::{fs, error::Error};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("Contents: \n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
@@ -21,5 +25,30 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
         Ok(Config { query, filename })
+    }
+}
+pub fn search<'a>(query: &str, contents: &'a str)-> Vec<&'a str>{
+    let mut result = Vec::new();
+    for i in contents.lines(){
+        if i.contains(query) {
+            result.push(i);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use babygrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|error| {
         println!("Problem reading arguments: {}", error);
         process::exit(-1);
     });
-    println!("Searching for: {}", &config.query);
-    println!("In file: {}", &config.filename);
+
     if let Err(e) = babygrep::run(config){
         println!("Application error: {}", e);
         process::exit(-1);
